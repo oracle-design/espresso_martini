0a461cd1c067a0e109c6d61d1fb0a2c99c8e36bf
FunnyQ <funnyq@funnyq.com>
2017-10-26 21:46:24 +0800

    :sparkles: added actions locale file generator

---
+++ lib/generators/martini/locale/actions/actions_generator.rb
@@ -0,0 +1,15 @@
+module Martini
+  module Locale
+    module Generators
+      class ActionsGenerator < Rails::Generators::Base
+        source_root File.expand_path('../templates', __FILE__)
+
+        argument :language, type: :string, default: 'en'
+
+        def generate_locale
+          template "locale_template_actions.yml", File.join('config/locales/actions', file_path, "#{language}.yml")
+        end
+      end
+    end
+  end
+end
