da64b1773ce7297a089b3ffcc1e9e2988912248a
FunnyQ <funnyq@gmail.com>
2018-01-05 17:41:48 +0800

    :sparkles: added vue initializer generator

---
+++ lib/generators/espresso/vue/initializer/initializer_generator.rb
@@ -0,0 +1,17 @@
+module Espresso
+  module Vue
+    module Generators
+      class InitializerGenerator < Rails::Generators::NamedBase
+        source_root File.expand_path('../templates', __FILE__)
+
+        argument :scope_name, type: :string, default: 'application'
+
+        def generate_initializer
+          initializer_path = name.split('::')[0..-2].join('/')
+
+          template 'initializer_template.js', File.join("app/javascript/src/#{scope_name}/js/vue_initializers/", initializer_path, "#{file_name}.js")
+        end
+      end
+    end
+  end
+end
