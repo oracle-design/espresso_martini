fdb288114c4d3d45963f9737cda0cbae23979517
FunnyQ <funnyq@gmail.com>
2018-04-12 12:54:22 +0800

    :bug:

--- lib/generators/espresso/vue/component/component_generator.rb
+++ lib/generators/espresso/vue/component/component_generator.rb
@@ -9,12 +9,12 @@ module Espresso
         def generate_vue_component
           component_path = name.split('::')[0..-2].join('/')
 
-          template 'component_template.vue', File.join("app/javascript/src/#{scope_name}/js/components/", component_path.downcase, "#{file_name.dasherize}.vue")
-          template 'component_style_template.sass', File.join("app/javascript/src/#{scope_name}/css/components/", component_path.downcase, "#{file_name.dasherize}.sass")
+          template 'component_template.vue', File.join("app/javascript/src/#{scope_name}/js/components/", component_path.underscore, "#{file_name.dasherize}.vue")
+          template 'component_style_template.sass', File.join("app/javascript/src/#{scope_name}/css/components/", component_path.underscore, "#{file_name.dasherize}.sass")
         end
 
         def css_class_name
-          %(.vc-#{name.split('::').map(&:dasherize).join('-')})
+          %(.vc-#{name.split('::').map(&:underscore).map(&:dasherize).join('-')})
         end
       end
     end
