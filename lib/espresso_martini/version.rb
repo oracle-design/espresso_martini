f399705136d77f8af8a5226619ebf22c16d549b4
FunnyQ <funnyq@gmail.com>
2018-04-22 16:07:18 +0800

    :bug:

--- lib/espresso_martini/version.rb
+++ lib/espresso_martini/version.rb
@@ -1,3 +1,3 @@
 module EspressoMartini
-  VERSION = '0.6.6'
+  VERSION = '0.6.7'
 end
--- lib/generators/espresso/vue/form/form_generator.rb
+++ lib/generators/espresso/vue/form/form_generator.rb
@@ -7,13 +7,13 @@ module Espresso
         def generate_form
           file_path = name.split('::')[0..-2].join('/')
 
-          template 'forms/form_template.js', File.join("app/javascript/src/shared/forms/", file_path.downcase, "#{file_name.underscore}_form.js")
+          template 'forms/form_template.js', File.join("app/javascript/src/shared/forms/", file_path.underscore, "#{file_name.underscore}_form.js")
         end
 
         def import_path
           up_count = name.split('::').length - 1
 
-          return './form_base' if up_count < 2
+          return './form_base' if up_count < 1
 
           %('#{'../' * up_count}form_base')
         end
