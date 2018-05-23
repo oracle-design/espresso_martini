17a551a704d4107094e2f6a6bbd1c00e66319060
FunnyQ <funnyq@gmail.com>
2018-05-23 16:04:55 +0800

    :bug:

--- lib/generators/espresso/vue/policy/policy_generator.rb
+++ lib/generators/espresso/vue/policy/policy_generator.rb
@@ -9,7 +9,7 @@ module Espresso
         def generate_vue_policy
           template 'policies/policy_template.js', %(app/javascript/src/shared/policies/#{model_name.underscore}_policy.js)
 
-          prepend_to_file 'app/javascript/src/shared/policies/index.js', "import #{model_name.classify} from './#{model_name.underscore}_policy'\n"
+          prepend_to_file 'app/javascript/src/shared/policies/index.js', "import #{model_name.classify}Policy from './#{model_name.underscore}_policy'\n"
           insert_into_file 'app/javascript/src/shared/policies/index.js', "  #{model_name.underscore}: #{model_name.classify}Policy,\n", after: "const POLICIES = {\n"
         end
 
